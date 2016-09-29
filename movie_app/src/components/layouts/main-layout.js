13f1b1fbe0a7ec511594d4fb72d0300ab69af5c3
venkatesh <venkatesh.vanagala1989@gmail.com>
2016-09-29 19:30:21 +0530

    Seperated Header by Header component

--- movie_app/src/components/layouts/main-layout.js
+++ movie_app/src/components/layouts/main-layout.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Header from '../header/header'
 import '../../stylesheets/common/main'
 
 export default class MainLayout extends Component {
     render(){
         return(
             <div className='main-container'>
-                <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                    <div className="container nav-container">
-                       <div className="navbar-header">
-                          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                              <span className="sr-only">Toggle navigation</span>
-                              <span className="icon-bar"></span>
-                              <span className="icon-bar"></span>
-                              <span className="icon-bar"></span>
-                          </button>
-                          <a className="navbar-brand" href="#">
-                              <img src={require('../../images/logo.png')} alt="" />
-                          </a>
-                        </div>
-                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                          <form className="navbar-form navbar-right">
-                            <input type="text" className="form-control" placeholder="Search for movies/tv shows..." />
-                          </form>
-                      </div>
-                   </div>
-                </nav>
+                <Header/>
                 <div className="main-container">
                     <div className="col-lg-12">
                         <h1>Logo Nav by Start Bootstrap</h1>

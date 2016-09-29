cac747205183ae7082a1019eda631b34b986055b
venkatesh <venkatesh.vanagala1989@gmail.com>
2016-09-29 17:43:25 +0530

    header layout for main page

--- movie_app/src/components/layouts/main-layout.js
+++ movie_app/src/components/layouts/main-layout.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
-
-import '../../stylesheets/common/footer'
-
+import '../../stylesheets/common/main'
 
 export default class MainLayout extends Component {
     render(){
         return(
-            <div className='container'>
-              <header className="primary-header"></header>
-              <aside className="primary-aside">
-                <ul>
-                    <li><Link to="/" activeClassName="active">Home</Link></li>
-                    <li><Link to="/widgets" activeClassName="active">Widgets</Link></li>
-                </ul>
-              </aside>
-              <main>
-                  {this.props.children}
-              </main>
+            <div className='main-container'>
+                <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                    <div className="container">
+                       <div className="navbar-header">
+                          <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                              <span className="sr-only">Toggle navigation</span>
+                              <span className="icon-bar"></span>
+                              <span className="icon-bar"></span>
+                              <span className="icon-bar"></span>
+                          </button>
+                          <a className="navbar-brand" href="#">
+                              <img src={require('../../images/logo.png')} alt="" />
+                          </a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                          <ul className="nav navbar-nav">
+                              <li>
+                                  <a href="#">About</a>
+                              </li>
+                              <li>
+                                  <a href="#">Services</a>
+                              </li>
+                              <li>
+                                  <a href="#">Contact</a>
+                              </li>
+                          </ul>
+                      </div>
+                   </div>
+                </nav>
+                <div className="main-container">
+                  <div>
+                      <div className="col-lg-12">
+                          <h1>Logo Nav by Start Bootstrap</h1>
+                          <p>Note: You may need to adjust some CSS based on the size of your logo. The default logo size is  pixels.</p>
+                      </div>
+                  </div>
+                </div>
             </div>
         )
     }
--- movie_app/webpack.config.js
+++ movie_app/webpack.config.js
@@ -22,6 +22,10 @@ module.exports = {
         //Font-awesome
         { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
         { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+        // Support for *.png files.
+        { test: /\.png$/,  loader: 'file-loader' },
+        // Support for *.gif files.
+        { test: /\.gif$/,  loader: 'file-loader' },
         {
           test: /\.js$/,
           exclude: /node_modules/,
